761f94451e0ea7204246e6290c3b439b0b0d16e7
Fabian Koch <fabian.koch@tum.de>
2026-01-12 16:24:29 +0100

    create PlatformIO project structure

---
+++ PlatformIO-Code-Embedded/src/main.cpp
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+// Pins
+const int sensorPIN = A0;
+const int servoPIN  = 5;
+
+// Servo
+Servo myservo;
+int max_step_size = 4;
+int angle = 90; // starting position
+
+// PID variables
+double Setpoint = 15.0;   // distance from sensor to center [in cm]
+double Input;
+double Output;
+
+double errSum = 0.0;
+double lastErr = 0.0;
+
+// PID gains
+double kp = 5.0;
+double ki = 0;
+double kd = 0;
+
+// Timing
+const unsigned long Ts = 20;   // update interval in ms
+unsigned long lastUpdate = 0;
+
+void setup() {
+  pinMode(sensorPIN, INPUT);
+  myservo.attach(servoPIN);
+  myservo.write(angle);           // neutral position
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  unsigned long now = millis();
+  if (now - lastUpdate >= Ts) {
+    lastUpdate = now;
+
+    // ---- SENSOR ----
+    int raw = analogRead(sensorPIN);
+    float voltage = raw * (5.0 / 1023.0);
+    //voltage = constrain(voltage, 0.45, 2.8);     // contrain voltage
+    Input = 27.86 * pow(voltage, -1.15);
+
+    // ---- PID ----
+    double dt = Ts / 1000.0;   // seconds
+
+    double error = Setpoint - Input;
+
+    errSum += error * dt;
+    errSum = constrain(errSum, -50, 50);  // anti-windup
+
+    double dErr = (error - lastErr) / dt;
+    lastErr = error;
+
+    Output = kp * error + ki * errSum + kd * dErr;
+
+    // ---- SERVO ----
+    int targetAngle = constrain(90 + Output, 0, 180);
+    int delta = targetAngle - angle;
+    delta = constrain(delta, -max_step_size, max_step_size);
+    angle += delta;
+    myservo.write(angle);
+
+    // ---- DEBUG ----
+    Serial.print("Dist: ");
+    Serial.print(Input);
+    Serial.print("  Out: ");
+    Serial.print(Output);
+    Serial.print("  Angle: ");
+    Serial.println(angle);
+  }
+  
+  myservo.write(angle);
+}
